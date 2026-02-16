1d4a628518aa1a6237cff700a4d067bc35dd76dd
kamaljeet <kamaljeet8130@gmail.com>
2026-02-16 20:28:05 +0530

    login test script from scratch

--- project-01-login/automation/src/test/LoginTest.java
+++ project-01-login/automation/src/test/LoginTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class LoginTest {
     public static void main(String[] args) throws InterruptedException {
@@ -14,13 +15,39 @@ public class LoginTest {
 
     }
     private static void validUserLogin(WebDriver driver , String url ,By userNameLocator,By passwordLocator,By submitButtonLocator) throws InterruptedException {
-        driver.get(url);
-        WebElement userNameInput = driver.findElement(userNameLocator);
-        WebElement passwordInput = driver.findElement(passwordLocator);
-        userNameInput.sendKeys("student");
-        passwordInput.sendKeys("Password123");
-        WebElement submitButton  = driver.findElement(submitButtonLocator);
-        submitButton.click();
-        driver.quit();
+       driver.get(url);
+       driver.findElement(userNameLocator).sendKeys("student");
+       driver.findElement(passwordLocator).sendKeys("Password123");
+       driver.findElement(submitButtonLocator).click();
+
+        String actualUrl = driver.getCurrentUrl();
+        if(actualUrl!=null && actualUrl.equals(url)){
+           throw new RuntimeException("URL Validation Failed");
+       }
+       String expectedTitle = "Logged In Successfully | Practice Test Automation";
+       String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle,"Title Validation Failed");
+        assert actualTitle != null;
+        System.out.println("actual Title length  : "  + actualTitle.length() + " :::: " + "expected length : " + expectedTitle.length());
+//       if(actualTitle!=null && actualTitle.equals(expectedTitle)){
+//           throw new RuntimeException(
+//                   "Title Validation Failed\n" +
+//                           "Actual: [" + actualTitle + "]\n" +
+//                           "Expected: [" + expectedTitle + "]"
+//           );
+//       }
+       WebElement header = driver.findElement(By.tagName("h1"));
+       if(!header.getText().equals("Logged In Successfully")){
+           throw new RuntimeException("Header Validation Failed");
+       }
+       WebElement logoutBtn = driver.findElement(By.linkText("Log out"));
+       if(!logoutBtn.isDisplayed()){
+           throw new RuntimeException("Logout button not visible");
+       }
+        System.out.println("All validation passed ");
+
     }
+    private static void
 }
+
+
