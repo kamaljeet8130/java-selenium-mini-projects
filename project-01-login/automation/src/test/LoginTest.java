a1858f2815b3d74bdefc03f93c6f2328424f323c
kamaljeet8130 <kamaljeet8130@gmail.com>
2026-02-19 18:02:48 +0530

    wip

--- project-01-login/automation/src/test/LoginTest.java
+++ project-01-login/automation/src/test/LoginTest.java
@@ -2,7 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LoginTest {
     static By userNameLocator = By.cssSelector("input#username");
@@ -13,54 +19,52 @@ public class LoginTest {
 
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         validUserLogin(driver,"student","Password123");
-//        testLogin_WithValidCredentials_ShouldDisplaySuccessMessage(driver, url, );
+        testLogin_WithValidCredentials_ShouldDisplaySuccessMessage(driver,"student","Password123");
+        testLogin_WithInvalidUsername_ShouldShowUsernameError(driver,"dfdf","Password123");
+        tearDown(driver);
 
     }
-
     private static void validUserLogin(WebDriver driver, String userName,String Password) throws InterruptedException {
         driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(),url,"URL Validation Failed : actual Url");
         driver.findElement(userNameLocator).sendKeys(userName);
         driver.findElement(passwordLocator).sendKeys(Password);
         driver.findElement(submitButtonLocator).click();
-
-        String actualUrl = driver.getCurrentUrl();
-        if (actualUrl != null && actualUrl.equals(url)) {
-            throw new RuntimeException("URL Validation Failed");
-        }
         String expectedTitle = "Logged In Successfully | Practice Test Automation";
-        String actualTitle = driver.getTitle();
-        Assert.assertEquals(actualTitle, expectedTitle, "Title Validation Failed");
-        assert actualTitle != null;
-        System.out.println("actual Title length  : " + actualTitle.length() + " :::: " + "expected length : " + expectedTitle.length());
-//       if(actualTitle!=null && actualTitle.equals(expectedTitle)){
-//           throw new RuntimeException(
-//                   "Title Validation Failed\n" +
-//                           "Actual: [" + actualTitle + "]\n" +
-//                           "Expected: [" + expectedTitle + "]"
-//           );
-//       }
+        Assert.assertEquals(driver.getTitle(),expectedTitle,"Title Validation Failed" );
         WebElement header = driver.findElement(By.tagName("h1"));
-        if (!header.getText().equals("Logged In Successfully")) {
-            throw new RuntimeException("Header Validation Failed");
-        }
+        Assert.assertEquals(header.getText(),"Logged In Successfully","Header Validation Failed");
         WebElement logoutBtn = driver.findElement(By.linkText("Log out"));
-        if (!logoutBtn.isDisplayed()) {
-            throw new RuntimeException("Logout button not visible");
-        }
+        Assert.assertTrue(logoutBtn.isDisplayed() ,"Log out button not visible");
         System.out.println("All validation passed ");
-
     }
 
-    private static void testLogin_WithValidCredentials_ShouldDisplaySuccessMessage(WebDriver driver, String url, By userNameLocator, By passwordLocator, By submitButtonLocator) {
+    private  static void testLogin_WithValidCredentials_ShouldDisplaySuccessMessage(WebDriver driver,String userName , String password){
         driver.get(url);
-        driver.findElement(userNameLocator).sendKeys("student");
-        driver.findElement(passwordLocator).sendKeys("Password123");
+        driver.findElement(userNameLocator).sendKeys(userName);
+        driver.findElement(passwordLocator).sendKeys(password);
         driver.findElement(submitButtonLocator).click();
+        WebElement header = driver.findElement(By.tagName("h1"));
+        String headerText = header.getText();
+        Assert.assertEquals(headerText,"Logged In Successfully","header validation Failed");
+    }
 
-        WebElement header
-                = driver
-                .findElement(By.tagName("h1"));
+    private static void testLogin_WithInvalidUsername_ShouldShowUsernameError(WebDriver driver,String userName,String password){
+        driver.get(url);
+        driver.findElement(userNameLocator).sendKeys(userName);
+        driver.findElement(passwordLocator).sendKeys(password);
+        driver.findElement(submitButtonLocator).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement errorMessage = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div#error"))
+        );
+        System.out.println( " error message :::: \n" +
+                errorMessage.getText() );
+    }
+    public static void tearDown(WebDriver driver){
+        driver.quit();
     }
 }
 
