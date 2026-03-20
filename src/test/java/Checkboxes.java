7be36dd3e6202759fd4f76981dd68ab28de43642
kamaljeet8130 <kamaljeet8130@gmail.com>
2026-03-20 18:38:38 +0530

    wip for checkbox page testcases

--- src/test/java/Checkboxes.java
+++ src/test/java/Checkboxes.java
@@ -10,21 +10,73 @@ public class Checkboxes extends BaseTest {
     final String CHECKBOX_ENDPOINT = "/checkboxes";
     By checkboxesLocator = By.cssSelector("#checkboxes input[type='checkbox']");
 
+    private List<WebElement> getCheckboxes(){
+        return driver.findElements(checkboxesLocator);
+    }
+    private int getCheckboxCount(){
+        List<WebElement> checkboxes= getCheckboxes();
+        return checkboxes.size();
+    }
+    private int getCheckedCheckboxCount(){
+        int count = 0;
+        for(WebElement checkbox:getCheckboxes()){
+            if(checkbox.isSelected()){
+                count++;
+            }
+        }
+        return count;
+    }
+    private int getUncheckedCheckboxCount(){
+        int count = 0;
+        for(WebElement checkbox:getCheckboxes()){
+            if(!checkbox.isSelected()){
+                count++;
+            }
+        }
+        return count;
+    }
+    private void checkedUncheckedCheckbox(){
+        for(WebElement checkbox:getCheckboxes()){
+            if(!checkbox.isSelected()){
+                checkbox.click();
+            }
+        }
+    }
+    private void uncheckedCheckedCheckbox(){
+        for(WebElement checkbox:getCheckboxes()){
+            if(checkbox.isSelected()){
+                checkbox.click();
+            }
+        }
+    }
 
     @Test
     public void verifyCheckboxCount(){
         navigate(CHECKBOX_ENDPOINT);
-        List<WebElement> checkboxes = driver.findElements(checkboxesLocator);
-        Assert.assertEquals(checkboxes.size(),2," Exactly 2 checkbox are not present on page");
+        Assert.assertEquals(getCheckboxes().size(),2," Exactly 2 checkbox are not present on page");
     }
 
     @Test
     public void verifyInitialState(){
         navigate(CHECKBOX_ENDPOINT);
-        List<WebElement> checkboxes = driver.findElements(checkboxesLocator);
-        Assert.assertFalse(checkboxes.get(0).isSelected(),"checkbox 1 is not unchecked initially");
-        Assert.assertTrue(checkboxes.get(1).isSelected(),"checkbox 2 is not checked initially");
-
+        int checkedCount = 0,uncheckedCount=0;
+        for (WebElement checkbox:getCheckboxes()){
+            if(checkbox.isSelected()){
+                checkedCount++;
+            }
+            else{
+                uncheckedCount++;
+            }
+        }
+        Assert.assertEquals(checkedCount,1,"There should be exactly 1 checked checkbox");
+        Assert.assertEquals(uncheckedCount,1,"There should be exactly 1 unchecked checkbox");
     }
 
+    @Test
+    public void verifyCheckingUnCheckCheckbox(){
+        navigate(CHECKBOX_ENDPOINT);
+        checkedUncheckedCheckbox();
+        Assert.assertEquals(getCheckedCheckboxCount(),2);
+
+    }
 }
