3c4269c9da32999cac1eb3ef6a482302698bf2d9
kamaljeet8130 <kamaljeet8130@gmail.com>
2026-09-21 00:33:17 +0530

    Explicit wait conditions updated for demo

--- src/main/java/webdrivermethods/waitmethods/ExplicitWaitsDemo.java
+++ src/main/java/webdrivermethods/waitmethods/ExplicitWaitsDemo.java
@@ -5,8 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
@@ -20,6 +22,29 @@ public class ExplicitWaitsDemo {
                         visibilityOfElementLocated(
                 By.xpath("//input[@placeholder=\"Username\"]")));
         txtUserName.sendKeys("Admin");
+        WebElement txtPassword = myWaits.until(
+                ExpectedConditions.
+                        visibilityOfElementLocated(
+                                By.xpath("//input[@placeholder=\"Password\"]")
+                        )
+        );
+        txtPassword.sendKeys("admin123");
 
+        WebElement loginButton = myWaits.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()=\" Login \"]")));
+        loginButton.click();
+        WebElement dashboardText = myWaits.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span/h6[text()=\"Dashboard\"]")));
+        boolean isTextDisplayed = dashboardText.isDisplayed();
+        Assert.assertTrue(isTextDisplayed);
+        driver.quit();
     }
 }
+/*
+explicit wait :
+first declaration and then use
+Declared by WebDriverWaits class with having constructor which require 2 args , driver and time
+
+.. it is conditional based it work more effectively
+finding element is inclusive as it is generic type
+it will wait for condition to be true , and the consider time
+we need to write multiple statement for multiple elements
+ */
