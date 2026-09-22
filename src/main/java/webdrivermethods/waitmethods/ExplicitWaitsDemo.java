3d81b8a136c144754e14944f7c4e4a2e55327cb3
kamaljeet8130 <kamaljeet8130@gmail.com>
2026-09-23 02:19:26 +0530

    remove extra import statement

--- src/main/java/webdrivermethods/waitmethods/ExplicitWaitsDemo.java
+++ src/main/java/webdrivermethods/waitmethods/ExplicitWaitsDemo.java
@@ -1,11 +1,9 @@
 package webdrivermethods.waitmethods;
 
-import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
