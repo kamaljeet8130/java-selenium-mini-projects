47fed1b0520913e95e387edb0a26cecc02ad7dcb
kamaljeet8130 <kamaljeet8130@gmail.com>
2026-09-23 02:17:54 +0530

    Conditional Methods in selenium

--- src/main/java/webdrivermethods/conditionalmethods/ConditionalMethods.java
+++ src/main/java/webdrivermethods/conditionalmethods/ConditionalMethods.java
@@ -1,6 +1,5 @@
-package webdrivermethods;
+package webdrivermethods.conditionalmethods;
 
-import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
